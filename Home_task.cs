bd81741382a25d4f17b0d9ef5739cc278df7d66c
Leonora Zharova <norka_200@mail.rugit config --global user.namegit config --global user.email norka_200@mail.ru>
2022-06-19 16:01:05 +0300

    Project is finished!

--- Home_task.cs
+++ Home_task.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrey = new string[6];
+
+FillArrey(arrey);
+
+string[] arreyResult = new string[CountElements(arrey)];
+
+FillResult(arrey, arreyResult);
+
+PrintArrey(arrey);
+Console.Write("-->");
+PrintArrey(arreyResult);
+
+void PrintArrey(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + " ");
+    Console.Write("]");
+}
+
+void FillArrey(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Please, enter a string : ");
+        arr[i] = Convert.ToString(Console.ReadLine());
+    }
+}
+
+int CountElements(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+void FillResult(string[] arr, string[] arrResult)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arrResult[count] = arr[i];
+            count++;
+        }
+    }
+}
+
+
+
+
+
+
+
